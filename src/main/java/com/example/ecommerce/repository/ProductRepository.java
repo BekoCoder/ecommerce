2d6b271626fd3c1f6c073077ea37f97492fc3091
BekoCoder <bekzodmirzayev35@gmail.com>
2024-07-18 08:36:27 +0500

    change 1

--- src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -2,9 +2,6 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 public interface ProductRepository  extends JpaRepository<ProductEntity, Long> {
 
