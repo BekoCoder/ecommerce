ef43b6b1d2c3cf4667cbed6696b5e249ae704129
BekoCoder <bekzodmirzayev35@gmil.com>
2024-07-28 11:23:28 +0500

    some bug fix 1

--- src/main/java/com/example/ecommerce/controller/DemoController.java
+++ src/main/java/com/example/ecommerce/controller/DemoController.java
@@ -1,15 +1,15 @@
-//package com.example.ecommerce.controller;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/auth/demo-controller")
-//public class DemoController {
-//    @GetMapping
-//    public ResponseEntity<String> demo() {
-//        return ResponseEntity.ok("Hello World");
-//    }
-//}
+package com.example.ecommerce.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/demo-controller")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> demo() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
